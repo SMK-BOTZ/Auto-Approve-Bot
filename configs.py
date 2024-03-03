d0da578e4b378337c255260b1bc14fb22952ffdb
SMK THE BOTZ <96283395+SMK-BOTZ@users.noreply.github.com>
2024-03-03 13:12:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,14 +1,14 @@
 from os import path, getenv
 
-SUDO = "5171347305"
+SUDO = "6827783925"
 
 class Config:
-    API_ID = int(getenv("API_ID", "14055090"))
-    API_HASH = getenv("API_HASH", "a46f7b439d0afa45b7a69fc450f754e9")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:AAFk0OhEkbFuUMrSSs6dfLdBYhxUI0c9Pt4")
-    FSUB = getenv("FSUB", "EmoBotDevolopers")
-    CHID = int(getenv("CHID", "-1001551504918"))
+    API_ID = int(getenv("API_ID", "28243586"))
+    API_HASH = getenv("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6812106698:AAGtDaint6SnowdD8tvgIEktg6Q-hzzhg50")
+    FSUB = getenv("FSUB", "SMK_NETWORK_0")
+    CHID = int(getenv("CHID", "-1002092889672"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jyotimaurya891824:AQOW8F91e1FhF7Ty@cluster0.m5988ba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
